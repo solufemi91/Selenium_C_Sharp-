1372e07bc62e99df80271517a3c2e97f2e6dcee3
Steven <stevenoo@spartaglobal.com>
2018-03-22 21:51:03 +0000

    added code as shown on youtube

--- Selenium_C_Sharp/Program.cs
+++ Selenium_C_Sharp/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,9 @@ namespace Selenium_C_Sharp
     {
         static void Main(string[] args)
         {
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("https://www.google.com");
         }
     }
 }
